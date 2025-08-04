cb24f52c1a4241f280a03bf079311ae2f572d5de
mrzarbab09-ai <mrzarbab09@gmail.com>
2025-08-04 21:49:22 +0500

    My Project

---
+++ Desktop/test/test 2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 9 * 76;
+    int b = 89;
+    cout << "What is the sum?\n";
+    cout << a << " + " << b << " = " << (a + b) << endl;
+    return 0;
+} 
